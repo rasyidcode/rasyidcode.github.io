ea63ee5f9bba58ee9b1d25d47fdab3f6d284a5b5
rasyidcode <rasyidcode7@gmail.com>
2026-01-13 10:01:52 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,38 @@
 export default function Page() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <div>
+      {/* Header */}
+      <header>
+        <h1 className="text-xl font-bold">Jamil</h1>
+        <button>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M4 6h16M4 12h16M4 18h16"
+            />
+          </svg>
+        </button>
+      </header>
+
+      {/* Hero */}
+      <section></section>
+
+      {/* Projects */}
+      <section></section>
+
+      {/* Contact */}
+      <section></section>
+
+      {/* Footer */}
+      <section></section>
+    </div>
+  );
 }
